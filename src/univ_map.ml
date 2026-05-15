689e2cb42b9d83d75d5c84d51d3baffb8875f458
public-release <as-public-release@janestreet.com>
2026-05-15 15:52:48 +0100

    v0.18~preview.130.100+614

--- src/univ_map.ml
+++ src/univ_map.ml
@@ -146,7 +146,11 @@ struct
   ;;
 
   let find_packed_by_id = Map.find
-  let find_packed_by_id_exn = Map.find_exn
+
+  let find_packed_by_id_exn ?(here = Stdlib.Lexing.dummy_pos) t key =
+    Map.find_exn ~here t key
+  ;;
+
   let type_equal : ('s t, 's Packed.t Map.M(Type_equal.Id.Uid).t) Type_equal.t = T
 end
 
